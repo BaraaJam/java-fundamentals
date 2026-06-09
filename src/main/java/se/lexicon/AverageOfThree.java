8f5c61e6b99e5f98d396352cefd073d052f17a1e
Baraa Jamos <Baraajamos@icloud.com>
2026-06-09 11:53:41 +0200

    feat: add average of three numbers exercise

---
+++ src/main/java/se/lexicon/AverageOfThree.java
@@ -0,0 +1,22 @@
+package se.lexicon;
+
+import java.util.Scanner;
+
+public class AverageOfThree {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int firstNumber = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int secondNumber = input.nextInt();
+
+        System.out.print("Enter third number: ");
+        int thirdNumber = input.nextInt();
+
+        double average = (double) (firstNumber + secondNumber + thirdNumber) / 3;
+
+        System.out.println("Average " + average);
+    }
+}
