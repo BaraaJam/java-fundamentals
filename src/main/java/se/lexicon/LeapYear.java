2f5e9dd0c6cc7454fb99c85f8baabe89c508a996
Baraa Jamos <Baraajamos@icloud.com>
2026-06-09 09:56:03 +0200

    feat: add leap year exercise

---
+++ src/main/java/se/lexicon/LeapYear.java
@@ -0,0 +1,29 @@
+package se.lexicon;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = input.nextInt();
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0){
+
+                    System.out.println(year + " is a leap year.");
+                } else {
+                    System.out.println(year + " is NOT a leap year.");
+                }
+            } else{
+                System.out.println(year + " is a leap year.");
+            }
+        } else {
+            System.out.println(year + " is NOT a leap year.");
+        }
+
+       input.close();
+    }
+}
