ac6057803acc115e3147dca4b89cd9b7d126e0f9
Baraa Jamos <Baraajamos@icloud.com>
2026-06-09 09:25:02 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/ProfileCard.java
@@ -0,0 +1,19 @@
+package se.lexicon;
+
+public class ProfileCard {
+    public void main() {
+
+        String name = "Baraa Jamos";
+        int age = 30;
+        String city = "Värnamo";
+
+        System.out.println("==================");
+        System.out.println("My Profile");
+        System.out.println("==================");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("City: " + city);
+        System.out.println("==================");
+
+    }
+}
