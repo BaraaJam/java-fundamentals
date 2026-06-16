a0336f3dd452f2151461c1afad38107113ddafa4
Baraa Jamos <Baraajamos@icloud.com>
2026-06-16 09:58:26 +0200

    feat(part2): add email slicer exercise

---
+++ src/main/java/se/lexicon/part2/EmailSlicer.java
@@ -0,0 +1,19 @@
+package se.lexicon.part2;
+
+import java.util.Scanner;
+
+public class EmailSlicer {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your email address: ");
+        String email = input.next();
+
+        int indexOfDomain = email.indexOf('@');
+        String domain = email.substring(indexOfDomain + 1);
+        String username = email.substring(0,indexOfDomain);
+
+        System.out.println("Username: " + username);
+        System.out.println("Domain  : " + domain);
+    }
+}
