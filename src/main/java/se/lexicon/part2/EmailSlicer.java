047c5f363b8f0b69b7160b57bfb9baff474d9061
Baraa Jamos <Baraajamos@icloud.com>
2026-06-16 10:02:37 +0200

    fix(part2): add @ validation and close scanner in email slicer

--- src/main/java/se/lexicon/part2/EmailSlicer.java
+++ src/main/java/se/lexicon/part2/EmailSlicer.java
@@ -10,10 +10,17 @@ public class EmailSlicer {
         String email = input.next();
 
         int indexOfDomain = email.indexOf('@');
-        String domain = email.substring(indexOfDomain + 1);
-        String username = email.substring(0,indexOfDomain);
 
-        System.out.println("Username: " + username);
-        System.out.println("Domain  : " + domain);
+        if (indexOfDomain == -1){
+            System.out.println("Invalid email address. Missing '@' symbol.");
+        } else {
+            String domain = email.substring(indexOfDomain + 1);
+            String username = email.substring(0,indexOfDomain);
+
+            System.out.println("Username: " + username);
+            System.out.println("Domain  : " + domain);
+        }
+
+        input.close();
     }
 }
